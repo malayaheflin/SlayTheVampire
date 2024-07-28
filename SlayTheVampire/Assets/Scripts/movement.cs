e5afbdd319728d5936c55692b3bfb2cc13a4506e
jmaloy315 <jmaloy@terpmail.umd.edu>
2024-07-28 16:58:51 -0400

    fixed first person movement with character controller

--- SlayTheVampire/Assets/Scripts/movement.cs
+++ SlayTheVampire/Assets/Scripts/movement.cs
@@ -4,15 +4,32 @@ public class movement : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 12f;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    Vector3 velocity;
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        }
+
         var xDir = Input.GetAxis("Horizontal");
         var zDir = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * xDir  + transform.forward * zDir;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 
 }
