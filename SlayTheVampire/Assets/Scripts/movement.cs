83eecd0e7deff18a46deff99dc27f0f817efa9c5
Malaya Heflin <malayaheflin@gmail.com>
2024-07-30 15:57:34 -0400

    jump
    added jump via input system package

--- SlayTheVampire/Assets/Scripts/movement.cs
+++ SlayTheVampire/Assets/Scripts/movement.cs
@@ -1,17 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
 
 public class movement : MonoBehaviour
 {
-    public CharacterController controller;
+    // eventually add animators & sound here too
+
     public float speed = 12f;
     public float gravity = -9.81f;
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
+    public float jumpHeight = 1.2f;
+
+    PlayerInput playerInput;
+    PlayerInput.MainActions input;
+
+    CharacterController controller;
     bool isGrounded;
 
+
     Vector3 velocity;
-    // Update is called once per frame
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+
+        playerInput = new PlayerInput();
+        input = playerInput.Main;
+        AssignInputs();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
@@ -30,6 +55,27 @@ public class movement : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    void OnEnable()
+    {
+        input.Enable();
+    }
+    void OnDisable()
+    {
+        input.Disable();
+    }
+
+    // for action mapping (input system package)
+    void AssignInputs()
+    {
+        input.Jump.performed += ctx => Jump();
+    }
+
+    void Jump()
+    {
+        if (isGrounded)
+            velocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
     }
 
 }
