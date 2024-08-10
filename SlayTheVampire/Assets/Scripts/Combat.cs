4490ee9efa77dc11ec393817c53cb8c0560cbadf
Malaya Heflin <malayaheflin@gmail.com>
2024-08-09 21:25:31 -0400

    added swing animation

--- SlayTheVampire/Assets/Scripts/Combat.cs
+++ SlayTheVampire/Assets/Scripts/Combat.cs
@@ -9,26 +9,38 @@ public class Combat : MonoBehaviour
     public float attackSpeed = 1f;
     public int attackDamage = 1;
     public LayerMask attackLayer;
-    public Camera cam; // get automatically ?
+    public Camera cam; // look into getting vars automatically instead of public
 
     bool attacking = false;
     bool readyToAttack = true;
 
+    public Animator animator;
+    private const string IDLESTATE = "Idle";
+    private const string PUNCHSTATE = "Attack";
+    string currentAnimationState;
+    
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            Attack();
+    }
+
     public void Attack()
     {
         if (!readyToAttack || attacking) return;
-
         readyToAttack = false;
         attacking = true;
 
         Invoke(nameof(ResetAttack), attackSpeed);
         Invoke(nameof(AttackRaycast), attackDelay);
+        ChangeAnimationState(PUNCHSTATE);
     }
 
     void ResetAttack()
     {
         attacking = false;
         readyToAttack = true;
+        ChangeAnimationState(IDLESTATE);
     }
 
     void AttackRaycast()
@@ -42,4 +54,15 @@ public class Combat : MonoBehaviour
             // }
         }
     }
+
+    // animation
+    public void ChangeAnimationState(string newState)
+    {
+        if (newState == currentAnimationState)
+            return;
+        
+        currentAnimationState = newState;
+        animator.CrossFadeInFixedTime(currentAnimationState, 0.2f);
+    }
+
 }
