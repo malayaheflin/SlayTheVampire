024a7376edbee328ba9336448dc2a4d55d3aefe5
jmaloy315 <jmaloy@terpmail.umd.edu>
2024-08-16 08:48:43 -0700

    added dashing

--- SlayTheVampire/Assets/Scripts/CharacterControl.cs
+++ SlayTheVampire/Assets/Scripts/CharacterControl.cs
@@ -1,9 +1,11 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CharacterControl : MonoBehaviour
 {
     // eventually add animators & sound here too
-
     public float speed = 12f;
     public float gravity = -30f;
     public Transform groundCheck;
@@ -11,8 +13,14 @@ public class CharacterControl : MonoBehaviour
     public LayerMask groundMask;
     public float jumpHeight = 1.2f;
     public KeyCode jumpKey = KeyCode.Space;
+    public float dashDistance = 5f;
     CharacterController controller;
     bool isGrounded;
+    private bool isDashing = false;
+    private float dashingPower = 20f;
+    private float dashingDuration= 0.3f;
+    private float dashingCooldown = 0f;
+
 
     Vector3 velocity;
 
@@ -26,12 +34,51 @@ public class CharacterControl : MonoBehaviour
 
     void Update()
     {
+        //Checking if grounded
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if(isGrounded && velocity.y < 0){
             velocity.y = -2f;
         }
 
+        //Dashing
+        if(Input.GetKey(KeyCode.W) && !isDashing)
+        {
+            if(Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                Debug.Log("Dash Forward");
+                StartCoroutine(Dash(transform.forward, Vector3.zero));
+            }
+        }
+
+        if(Input.GetKey(KeyCode.A) && !isDashing)
+        {
+            if(Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                Debug.Log("Dash Left");
+                StartCoroutine(Dash(Vector3.zero, transform.right * -1));
+            }
+        }
+
+        if(Input.GetKey(KeyCode.S) && !isDashing)
+        {
+            if(Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                Debug.Log("Dash Back");
+                StartCoroutine(Dash(transform.forward * -1, Vector3.zero));
+            }
+        }
+
+        if(Input.GetKey(KeyCode.D) && !isDashing)
+        {
+            if(Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                Debug.Log("Dash Right");
+                StartCoroutine(Dash(Vector3.zero, transform.right));
+            }
+        }
+
+        //Moving
         var xDir = Input.GetAxis("Horizontal");
         var zDir = Input.GetAxis("Vertical");
 
@@ -43,6 +90,7 @@ public class CharacterControl : MonoBehaviour
 
         controller.Move(velocity * Time.deltaTime);
 
+        //Jumping
         if(Input.GetKeyDown(jumpKey))
             Jump();
     }
@@ -51,5 +99,15 @@ public class CharacterControl : MonoBehaviour
     {
         if (isGrounded)
             velocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+    }
+
+    private IEnumerator Dash(Vector3 forward, Vector3 right)
+    {
+        isDashing = true;
+        velocity = forward * dashingPower + right * dashingPower;
+        yield return new WaitForSeconds(dashingDuration);
+        velocity = Vector3.zero;
+        yield return new WaitForSeconds(dashingCooldown);
+        isDashing = false;
     }
 }
